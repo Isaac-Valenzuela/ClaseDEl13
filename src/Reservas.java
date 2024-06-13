104cf6b1489d7f78228fec64da108e46f0b334ab
POO <isaacamaya941@gmail.com>
2024-06-13 08:00:43 -0500

    clase evento

--- src/Reservas.java
+++ src/Reservas.java
@@ -1,3 +1,5 @@
+import javax.naming.spi.ResolveResult;
+
 public class Reservas{
     private int id_reserva;
     private String cliente;
@@ -34,7 +36,54 @@ public class Reservas{
         this.fecha = fecha;
     }
 
-    private void calcularCosto(){
-
+    public void calcularCosto(){
     }
 }
+
+class Evento extends Reservas{
+    private String tipoevento;
+    private int costoevento;
+
+    public Evento(int id_reserva, String cliente, String fecha, String tipoevento, int costoevento ){
+        super(id_reserva, cliente, fecha);
+        this.tipoevento = tipoevento;
+        this.costoevento = costoevento;
+    }
+
+    public String getTipoevento() {
+        return tipoevento;
+    }
+
+    public void setTipoevento(String tipoevento) {
+        this.tipoevento = tipoevento;
+    }
+
+    public int getCostoevento() {
+        return costoevento;
+    }
+
+    public void setCostoevento(int costoevento) {
+        this.costoevento = costoevento;
+    }
+
+    @Override
+    public void calcularCosto(){
+        System.out.println("------------------------------------");
+        System.out.println("Reserva de Conferencia:");
+        System.out.println("Costo base de Conferencia: $1000");
+        System.out.println("Equipo audiovisual: $100 adicionales");
+        System.out.println("Servicio de catering: $200 adicionales");
+        System.out.println("------------------------------------");
+        System.out.println("Reserva de Boda");
+        System.out.println("Costo base de Boda: $2000");
+        System.out.println("Decoración floral: $500 adicionales.");
+        System.out.println("Música en vivo: $300 adicionales.");
+        System.out.println("------------------------------------");
+        System.out.println("Reserva de Concierto");
+        System.out.println("Costo base de Concierto: $1500");
+        System.out.println("Seguridad adicional: $400 adicionales");
+        System.out.println("Stand de comida: $150 adicionales.");
+    }
+}
+
+class Conferencia extends
