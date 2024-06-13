3ad39e0a3cfb8ff5d2baf4d107bccda2c7187f3c
DavidCatucuamba <ariel.catucuamba@epn.edu.ec>
2024-06-13 08:33:57 -0500

    Main Boda

--- src/SistemaReservas/Main.java
+++ src/SistemaReservas/Main.java
@@ -6,5 +6,9 @@ public class Main {
                 "Conferencia", 1000, 4, 200);
 
         c1.calcularCosto();
+        Boda b1 = new Boda(123,"PEPE", "22/11/2029",
+                "Boda", 2000, "Cutuglagua", 300, 500 );
+
+        b1.calcularCosto();
     }
 }
